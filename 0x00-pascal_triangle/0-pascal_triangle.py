33ed2cbe8d429f4051becee231e02223e2cd5547
adil mae <itsadilhadid@gmail.com>
2024-01-04 21:02:59 +0100

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""Pascal Triangle"""
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1] * (i + 1) for i in range(n)]
-
-    for i in range(2, n):
-        for j in range(1, i):
-            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-
-    return triangle
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    """Pascal Triangle"""
+    result = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            c = 1
+            for j in range(1, i + 1):
+                level.append(c)
+                c = c * (i - j) // j
+            result.append(level)
+    return result
