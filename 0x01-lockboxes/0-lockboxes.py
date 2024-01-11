ab9b4fbf5e44a46984c34f2d2d93731f381c66fa
Adil Mae <itsadilhadid@gmail.com>
2024-01-11 18:39:35 +0100

    lock boxe solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,11 +5,11 @@
 def canUnlockAll(boxes):
     """ lockboxes problem"""
     for i in range(1, len(boxes)):
-        f = False
+        track = False
         for box in range(len(boxes)):
-            if k in boxes[box] and box != i:
-                f = True
+            if i in boxes[box] and box != i:
+                track = True
                 break
-        if not f:
+        if not track:
             return False
     return True
