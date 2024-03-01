ba89bd701527c603f93dffb52c795929504f4ac0
adilmae <itsadilhadid@gmail.com>
2024-03-01 01:44:44 +0100

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,20 +3,18 @@
 
 
 def makeChange(coins, total):
-    """ making change function """
-
-def makeChange(coins, total):
+    """ change needed to reach total"""
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
